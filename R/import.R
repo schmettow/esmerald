be9b4b8dcfb7e3f1e185307a178ba2ee868a54f3
M Schmettow <schmettow@web.de>
2021-02-26 12:16:21 +0100

    + removed bayr dependency + read_ethica_csv Item --> Item_raw

--- R/import.R
+++ R/import.R
@@ -16,14 +16,16 @@ read_ethica_csv <- function(file)
            Device = `Device ID`,
            T_Scheduled = `Scheduled Time`,
            T_Issued = `Issued Time`,
-           T_Response = `Response Time`,
-           duration = `Duration (minutes)`) %>%
+           T_Response = `Response Time`) %>%
     mutate(Action = file) %>%
     mutate_at(vars(starts_with("T_")), lubridate::as_datetime) %>%
     mutate(duration = T_Response - T_Issued) %>%
-    pivot_longer(starts_with("["), names_to = "Item", values_to = "Response") %>%
-    mutate(response = as.numeric(str_match(Response, "[0-9]+"))) %>%
-    bayr::as_tbl_obs()
+    pivot_longer(starts_with("["), names_to = "Item_raw", values_to = "Response") %>%
+    mutate(response = as.numeric(str_match(Response, "[0-9]+")),
+           Obs = row_number()) %>%
+    select(Obs, Action, Part, Device,
+           T_Scheduled, T_Issued, T_Response,
+           Item_raw, Response, response)
   class(out) <- c("tbl_esm", class(out))
   out
 }
@@ -37,9 +39,10 @@ print.tbl_esm <-
     n_Obs <- nrow(x)
     n_shown <- min(8, nrow(x))
     n_Part <- nrow(distinct(x,Part))
-    n_Item <- nrow(distinct(x,Item))
+    n_Item <- nrow(distinct(x,Item_raw))
     n_Scheduled <- nrow(distinct(x,T_Scheduled))
-    cap = stringr::str_c("Table: ESM data with ", n_Obs, " observations (",
+    cap = stringr::str_c("Table: ESM data with ",
+                         n_Obs, " observations (",
                          n_shown," shown), ",
                          n_Part, " participants and ",
                          n_Item, " items")
@@ -47,3 +50,19 @@ print.tbl_esm <-
     sample_n(x, 8) %>% base:::print.data.frame()
     invisible(x)
   }
+
+#' @rdname read_ethica_csv
+#' @export
+
+export_item_labels <- function(tbl_esm){
+    tbl_esm %>%
+    select(Item_raw) %>%
+    distinct() %>%
+    mutate(Scale = "",
+           Item = "",
+           Label = "",
+           min = "",
+           max = "",
+           reverse = T)
+  }
+
